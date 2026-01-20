1b281e8072372b2992f65c65336956d4d8c08a0f
jakeulous <jake.giangan@gmail.com>
2026-01-20 11:57:45 +0800

    build: install ShadCN and TailwindCSS to project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div>
-      Start here
+    <div className='flex w-dvw h-dvh bg-slate-400'>
+      <div className="flex w-full justify-center bg-slate-200 mx-48">
+        Start Here
+      </div>
     </div>
   );
 }
